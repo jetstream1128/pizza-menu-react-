e68379ef088aa97365038ec69e099ae0d6d687bf
jetstream1128 <bogdanalexeev2007@live.com>
2023-06-20 12:55:24 -0600

    1

--- src/index.js
+++ src/index.js
@@ -49,12 +49,41 @@ const pizzaData = [
 function App() {
 	return (
 		<div>
-			<h2>Hello react!</h2>
+			<Header />
+			<Menu />
+			<Footer />
+		</div>
+	);
+}
+
+function Header() {
+	return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu() {
+	return (
+		<div>
+			<h2>Our menu</h2>
 			<Pizza />
 			<Pizza />
 			<Pizza />
 		</div>
 	);
+}
+function Footer() {
+	const hour = new Date().getHours();
+	const openHour = 12;
+	const closeHour = 22;
+
+	const isOpen = hour >= openHour && hour <= closeHour;
+	console.log(isOpen);
+
+	// alert("We're currently open!");
+	// else alert("Sorry we're closed");
+
+	return (
+		<footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
+	);
+	// return React.createElement("footer", null, "We're currently open!");
 }
 
 function Pizza() {
